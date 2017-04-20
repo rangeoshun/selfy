4cdde5af7f3c323354071663fcab952477228d3f
Abel VARGA <rangeoshun@gmail.com>
2017-04-20 08:20:38 +0200

    Remove matching, fix naive breake

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ let LEX = [[]];
 
 function arr ( item )
 {
-  return item instanceof Array ? item : []; 
+  return item instanceof Array ? item : [];
 }
 
 function pattern ( _str )
@@ -32,13 +32,14 @@ function naiveBreak ( _str )
   const length = str.length;
   let patterns = [];
   let i = length;
-  let k = 0;
+  let k;
 
   for (; i > 0; i--)
   {
     const sub = str.slice(0, i);
     patterns = patterns.concat(pattern(sub));
-    
+    k = 0;
+
     for (; k < i; k++)
     {
       const subSub = sub.slice(k, i);
@@ -53,10 +54,11 @@ function equals ( _a, _b )
 {
   const a = arr(_a);
   const b = arr(_b);
-console.log(a,b, a.join(',') === b.join(','))
+
   return (
-    a.length && 
+    a.length &&
     b.length &&
+    a.length === b.length &&
     a.join(',') === b.join(',')
   );
 }
@@ -86,12 +88,12 @@ function onlyUnique ( _list )
   return list.reduce(
     (acc, item) => (
       isUnique(arr(item), acc)
-        ? acc.concat(item)
+        ? acc.concat([item])
         : acc
     ),
     []
   );
-} 
+}
 
 function toPattern ( _acc, _str )
 {
@@ -169,87 +171,12 @@ function normalizeList ( _list, _lex )
   return list.map(( pattern ) => normalize(pattern, lex));
 }
 
-function matchSeries ( _s1, _s2 )
-{
-  const s1 = arr(_s1);
-  const start = s1.length | 0;
-  const s2 = arr(_s2);
-  const k = s2.length - (s1.length - start);
-  let verdict = true;
-  let i = start;
-
-  for (; i > 1; i--)
-  {
-    if (s1[i] != s2[k])
-    {
-      verdict = false;
-      break;
-    }
-  }
-
-  return verdict;
-}
-
 function byAscLength ( _one, _two )
 {
-  const one = arr(_one).length || 0;
-  const two = arr(_two).length || 0;
+  const one = arr(_one).length | 0;
+  const two = arr(_two).length | 0;
 
   return one - two;
-}
-
-function compareTwo ( _one, _two )
-{
-  const one = arr(_one);
-  const two = arr(_two);
-  let buff = [];
-
-  for (let i in one)
-  {
-    if (buff.length) break;
-    
-    const end = one.length - i;
-    
-    if (end < 2) break;
-    
-    const partOne = one.slice(0, end);
-
-    if (matchSeries(partOne, two)) {
-      buff = buff.concat([partOne]);
-    }
-  }
-
-  return buff;
-}
-
-function compareSeries ( _one )
-{
-  return function _comSer ( _buff, _two, _i )
-  {
-    return arr(_buff).concat(compareTwo(arr(_one), arr(_two)));
-  };
-}
-
-function toMatches ( _acc, _series, _i, _all )
-{
-  const all = arr(_all);
-
-  return arr(_acc)
-    .concat(
-      all
-        .slice((_i | 0) + 1, (all.length | 0))
-        .reduce(
-          compareSeries(arr(_series)),
-          []
-        )
-      );
-}
-
-function findMatches ( _arr )
-{
-  return arr(_arr)
-    .sort(byAscLength)
-    .reduce(toMatches, []);
 }
 
 function input ()
@@ -278,9 +205,6 @@ function handleInput (err, result)
 
   console.log(JSON.stringify(patterned));
   console.log(JSON.stringify(LEX));
-  console.log(JSON.stringify(
-    //findMatches(LEX.slice(1,LEX.length))
-  ));
 
   prompt.get(
     '>',
